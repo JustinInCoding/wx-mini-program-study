f79c55d7f4b4a215f51aaaeb532d540fb4a2eecf
Justin <justin.wang4@icloud.com>
2021-04-29 10:45:17 +0800

    [add] form complete

---
+++ pages/uploader/uploader.js
@@ -0,0 +1,87 @@
+// pages/uploader/uploader.js
+Page({
+  mixins: [require('../../mixin/themeChanged')],
+  /**
+   * Page initial data
+   */
+  data: {
+    files: []
+  },
+
+  chooseImage: function (e) {
+    var that = this;
+    wx.chooseImage({
+        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+        success: function (res) {
+            // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+            that.setData({
+                files: that.data.files.concat(res.tempFilePaths)
+            });
+        }
+    })
+  },
+
+  previewImage: function(e){
+    wx.previewImage({
+        current: e.currentTarget.id, // 当前显示图片的http链接
+        urls: this.data.files // 需要预览的图片http链接列表
+    })
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
