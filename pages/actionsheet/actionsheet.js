185a87318905328bc884686b0bc75784554dc78d
Justin <justin.wang4@icloud.com>
2021-04-29 11:17:23 +0800

    [add] search and navigator

---
+++ pages/actionsheet/actionsheet.js
@@ -0,0 +1,86 @@
+// pages/actionsheet/actionsheet.js
+Page({
+  mixins: [require('../../mixin/themeChanged')],
+  /**
+   * Page initial data
+   */
+  data: {
+    showIOSDialog: false,
+    showAndroidDialog: false,
+  },
+
+  close: function() {
+    this.setData({
+        showIOSDialog: false,
+        showAndroidDialog: false
+    });
+  },
+
+  openIOS: function () {
+    this.setData({
+        showIOSDialog: true
+    });
+  },
+
+  openAndroid: function () {
+      this.setData({
+          showAndroidDialog: true
+      });
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
